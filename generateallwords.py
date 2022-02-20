7ed0df121651fe8caa17d8c3673fd0eaef6e8a86
supernerdhelloworld <curiousjacob123@gmail.com>
2022-02-20 09:36:32 -0800

    Fix word generator & generate words

--- generateallwords.py
+++ generateallwords.py
@@ -1,51 +1,8 @@
-import sys
-original_stdout = sys.stdout # Save a reference to the original standard output
-
-# Python 3 program to print all
-# possible strings of length k
-
-# The method that prints all
-# possible strings of length k.
-# It is mainly a wrapper over
-# recursive function printAllKLengthRec()
-def printAllKLength(set, k):
-    n = len(set)
-    printAllKLengthRec(set, "", n, k)
-
-
-# The main recursive method
-# to print all possible
-# strings of length k
-def printAllKLengthRec(set, prefix, n, k):
-    # Base case: k is 0,
-    # print prefix
-    if (k == 0):
-        print(prefix)
-        return
-
-    # One by one add all characters
-    # from set and recursively
-    # call for k equals to k-1
-    for i in range(n):
-        # Next character of input added
-        newPrefix = prefix + set[i]
-
-        # k is decreased, because
-        # we have added a new character
-        printAllKLengthRec(set, newPrefix, n, k - 1)
-
-
-# Driver Code
-if __name__ == "__main__":
-
-    az = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    k = 5
-    with open('words.txt', 'w') as f:
-        sys.stdout = f  # Change the standard output to the file we created.
-        printAllKLength(az, k)
-        sys.stdout = original_stdout  # Reset the standard output to its original value
-
-
-# This code is contributed
-# by ChitraNayal
+LETTERS = ''.join(map(chr, range(ord('A'), ord('Z') + 1)))
+with open('words.txt', 'w') as fp:
+    for a in LETTERS:
+        for b in LETTERS:
+            for c in LETTERS:
+                for d in LETTERS:
+                    for e in LETTERS:
+                        fp.write(a + b + c + d + e + '\n')
